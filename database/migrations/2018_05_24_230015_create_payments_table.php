35eb363d911f3cbb751e901ba324b7729dccb16c
Colin J. Mills <colin.j.mills96@gmail.com>
2018-05-24 23:05:25 +0000

    Add payments table plus rebuild tickets as well

--- database/migrations/2018_05_24_230015_create_payments_table.php
+++ database/migrations/2018_05_24_230015_create_payments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTicketsTable extends Migration
+class CreatePaymentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateTicketsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tickets', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('paid_status');
-            $table->string('current_level');
-            $table->float('owing');
-            $table->unsignedInteger('parking_lot_id');
-            $table->foreign('parking_lot_id')->references('id')->on('parking_lots');
+            $table->string('name');
+            $table->string('stay_length');
+            $table->float('paid_amount');
+            $table->string('charge_id')
+            $table->unsignedInteger('ticket_id');
+            $table->foreign('ticket_id')->references('id')->on('tickets');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateTicketsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tickets');
+        Schema::dropIfExists('payments');
     }
 }
---
+++ database/migrations/2018_05_24_230256_create_ticket_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('parking_lot_id');
+            $table->unsignedInteger('payment_id');
+            $table->foreign('parking_lot_id')->references('id')->on('parking_lots');
+            $table->foreign('payment_id')->references('id')->on('payment');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
